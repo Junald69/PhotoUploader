cf3cc4c58f8d83cc7416711b35af9cb21762abc1
Junald69 <jj.n.dona@gmail.com>
2024-12-28 16:13:39 +0900

    Fix GoogleAuth initialization with environment variables

--- functions/upload.js
+++ functions/upload.js
@@ -4,16 +4,21 @@ const { Readable } = require('stream');
 exports.handler = async (event, context) => {
     try {
         if (event.httpMethod !== 'POST') {
-            return { statusCode: 405, body: 'Method Not Allowed' };
+            return {
+                statusCode: 405,
+                body: 'Method Not Allowed',
+            };
         }
 
+        // Google Drive API 認証
         const auth = new google.auth.GoogleAuth({
-            keyFile: 'service-account-key.json',
+            credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS),
             scopes: ['https://www.googleapis.com/auth/drive.file'],
         });
 
         const drive = google.drive({ version: 'v3', auth });
 
+        // アップロードされたファイルを解析
         const boundary = event.headers['content-type'].split('boundary=')[1];
         const body = Buffer.from(event.body, 'base64').toString('utf8');
         const parts = body.split(`--${boundary}`);
@@ -22,9 +27,10 @@ exports.handler = async (event, context) => {
         const fileContent = filePart.split('\r\n\r\n')[1];
         const fileName = filePart.match(/filename="(.+?)"/)[1];
 
+        // Google Drive にアップロード
         const fileMetadata = {
             name: fileName,
-            parents: ['15FiZVLx6y5sRftCe8a4pM9go8EP-a_wT'],
+            parents: ['15FiZVLx6y5sRftCe8a4pM9go8EP-a_wT'], // Google DriveフォルダID
         };
 
         const media = {
@@ -43,9 +49,11 @@ exports.handler = async (event, context) => {
             body: JSON.stringify({ id: file.data.id, name: file.data.name }),
         };
     } catch (error) {
+        console.error('エラー:', error);
         return {
             statusCode: 500,
             body: JSON.stringify({ error: error.message }),
         };
     }
 };
+
